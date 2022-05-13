115e3fc8634e76f18d6f01ce5d245432aa7589b7
mojyamojya32 <asato@flashcast.jp>
2022-05-13 11:14:46 +0900

    New Feature

--- Program01.java
+++ Program01.java
@@ -2,5 +2,6 @@ public class Program01 {
     public static void main(String[] args) {
         System.out.println("Program01");
         System.out.println("Function-01");
+        System.out.println("New Feature");
     }
 }
